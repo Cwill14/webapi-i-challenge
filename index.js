4e97559906ce3d9c8e19ac6a21c7470cb6408fda
Clark Williams <theclarkwilliams@gmail.com>
2019-08-05 14:41:08 -0600

    get /api/users working

--- index.js
+++ index.js
@@ -4,8 +4,41 @@ const express = require('express');
 const Data = require('./data/db.js');
 
 const server = express();
-
 server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    Data.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(error => {
+            res.status(500).json({  error: "The users information could not be retrieved." })
+        })
+})
+
+server.post('/api/users', (req, res) => {
+    Data.insert()
+        .then()
+        .catch()
+})
+
+server.get('/api/users/:id', (req, res) => {
+    Data.findById()
+        .then()
+        .catch()
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    Data.remove()
+        .then()
+        .catch()
+})
+
+server.put('/api/users/:id', (req, res) => {
+    Data.update()
+        .then()
+        .catch()
+})
 
 const port = 5000
 server.listen(port, () => console.log('server running at 5000'))
