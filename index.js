7b3176ebaedb0d3aab1bdeef855bf570098773a7
Clark Williams <theclarkwilliams@gmail.com>
2019-08-05 14:27:58 -0600

    server running

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // implement your API here
+const express = require('express');
+
+const Data = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+const port = 5000
+server.listen(port, () => console.log('server running at 5000'))
